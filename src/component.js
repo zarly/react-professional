ebd5a0fc347a781ab87ccde35c877660f35b2f3e
alex <rboots@ya.ru>
2018-02-05 22:31:49 +0300

    observable component

--- src/component.js
+++ src/component.js
@@ -4,12 +4,60 @@ const React = require('react');
 class Component extends React.Component {
 	constructor (...args) {
 		super(...args);
+		this._events = {};
 	}
 
-	on () {}
-	once () {}
-	off () {}
-	emit () {}
+	/**
+	 * Subscribe on the event
+	 *
+	 * @param {String} event
+	 * @param {Function} handler
+	 */
+	on (event, handler) {
+		if (!this._events[event]) this._events[event] = [];
+		this._events[event].push(handler);
+	}
+
+	/**
+	 * Subscribe on the event just once
+	 *
+	 * @param {String} event
+	 * @param {Function} handler
+	 */
+	once (event, handler) {
+		const proxyHandler = (...args) => {
+			this.off(event, proxyHandler);
+			return handler(...args);
+		};
+		this.on(event, proxyHandler);
+	}
+
+	/**
+	 * Unsubscribe on the event
+	 *
+	 * @param {String} event
+	 * @param {Function} handler
+	 */
+	off (event, handler) {
+		if (handler && this._events[event]) {
+			this._events[event] = this._events[event].filter(h => h !== handler);
+		} else {
+			this._events[event] = [];
+		}
+	}
+
+	/**
+	 * Emit the event with the data
+	 *
+	 * @param {String} event
+	 * @param {*} data
+	 */
+	emit (event, data) {
+		if (!this._events[event]) return;
+		this._events[event].forEach(function (handler) {
+			handler(data);
+		});
+	}
 }
 
 module.exports = Component;
--- tests/component.js
+++ tests/component.js
@@ -19,6 +19,77 @@ describe('Component', function () {
 		assert.equal(typeof component.off, 'function');
 		assert.equal(typeof component.emit, 'function');
 	});
+
+	it('should call event handler', function (done) {
+		const component = new Component();
+		component.on('some_event', done);
+		component.emit('some_event');
+	});
+
+	it('should not call event handler after off by handler', function () {
+		function handler () {
+			throw new Error('Should not be called');
+		}
+		const component = new Component();
+		component.on('some_event', handler);
+		component.off('some_event', handler);
+		component.emit('some_event');
+	});
+
+	it('should not call event handler after off without handler', function () {
+		function handler () {
+			throw new Error('Should not be called');
+		}
+		const component = new Component();
+		component.on('some_event', handler);
+		component.off('some_event');
+		component.emit('some_event');
+	});
+
+	it('should call proper event handler', function (done) {
+		function handler () {
+			throw new Error('Should not be called');
+		}
+		const component = new Component();
+		component.on('some_event', handler);
+		component.on('some_event', done);
+		component.off('some_event', handler);
+		component.emit('some_event');
+	});
+
+	it('should call proper event from multiple events', function (done) {
+		function handler () {
+			throw new Error('Should not be called');
+		}
+		const component = new Component();
+		component.on('proper_event', done);
+		component.on('other_event', handler);
+		component.emit('proper_event');
+	});
+
+	it('should call event once', function () {
+		var counter = 0;
+		function handler () {
+			assert.equal(++counter, 1);
+		}
+		const component = new Component();
+		component.once('some_event', handler);
+		component.emit('some_event');
+		component.emit('some_event');
+	});
+
+	it('should call event multiple times', function () {
+		var counter = 0;
+		function handler () {
+			counter++;
+		}
+		const component = new Component();
+		component.on('some_event', handler);
+		component.emit('some_event');
+		component.emit('some_event');
+		component.emit('some_event');
+		assert.equal(counter, 3);
+	});
 });
 
 
